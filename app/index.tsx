28a1a716252b0fd39aa02d1b52d345d4034af8e9
Ayush Soni <soni.ayush.2212@gmail.com>
2025-06-26 01:46:18 +0530

    Add nativewind

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center p-4">
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        voluptates saepe quas tempore tempora ut necessitatibus voluptate earum
+        ab, quisquam iusto esse, dolor consequatur doloremque corporis quidem
+        sit magnam itaque.
+      </Text>
+    </View>
+  );
+}
