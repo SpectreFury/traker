9ec1e529aa73557dbb1f5bd1ee28633b20a0df12
Ayush Soni <soni.ayush.2212@gmail.com>
2025-06-27 23:42:54 +0530

    Add login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,29 @@
-import { Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center p-4">
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        voluptates saepe quas tempore tempora ut necessitatibus voluptate earum
-        ab, quisquam iusto esse, dolor consequatur doloremque corporis quidem
-        sit magnam itaque.
-      </Text>
+    <View className="flex-1 items-center mt-40 px-4">
+      <View className="w-full items-center">
+        <Image
+          source={require("../assets/images/habit-icon.svg")}
+          style={{ width: 200, height: 200, marginBottom: 16 }}
+          contentFit="contain"
+        />
+        <Text className="font-opensans-bold text-4xl">Traker</Text>
+        <Text className="font-opensans-medium mt-2 text-xl">
+          Track your activities with your friends.
+        </Text>
+
+        <Pressable
+          className="px-3 py-4 border-slate-200 bg-slate-900 rounded mt-10"
+          onPress={() => console.log("Button Pressed")}
+        >
+          <Text className="font-opensans-medium text-slate-100">
+            Login With Google
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'opensans': ['OpenSans-Regular'],
+        'opensans-bold': ['OpenSans-Bold'],
+        'opensans-bold-italic': ['OpenSans-BoldItalic'],
+        'opensans-extrabold': ['OpenSans-ExtraBold'],
+        'opensans-extrabold-italic': ['OpenSans-ExtraBoldItalic'],
+        'opensans-italic': ['OpenSans-Italic'],
+        'opensans-light': ['OpenSans-Light'],
+        'opensans-light-italic': ['OpenSans-LightItalic'],
+        'opensans-medium': ['OpenSans-Medium'],
+        'opensans-medium-italic': ['OpenSans-MediumItalic'],
+        'opensans-semibold': ['OpenSans-SemiBold'],
+        'opensans-semibold-italic': ['OpenSans-SemiBoldItalic'],
+      },
+    },
   },
   plugins: [],
-}
+};
