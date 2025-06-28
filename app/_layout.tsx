c88800342b978d98130f89b83546597f2d8c337a
Ayush Soni <soni.ayush.2212@gmail.com>
2025-06-28 19:42:57 +0530

    Add tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,42 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+import "./globals.css";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "OpenSans-Regular": require("../assets/fonts/OpenSans-Regular.ttf"),
+    "OpenSans-Bold": require("../assets/fonts/OpenSans-Bold.ttf"),
+    "OpenSans-BoldItalic": require("../assets/fonts/OpenSans-BoldItalic.ttf"),
+    "OpenSans-ExtraBold": require("../assets/fonts/OpenSans-ExtraBold.ttf"),
+    "OpenSans-ExtraBoldItalic": require("../assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
+    "OpenSans-Italic": require("../assets/fonts/OpenSans-Italic.ttf"),
+    "OpenSans-Light": require("../assets/fonts/OpenSans-Light.ttf"),
+    "OpenSans-LightItalic": require("../assets/fonts/OpenSans-LightItalic.ttf"),
+    "OpenSans-Medium": require("../assets/fonts/OpenSans-Medium.ttf"),
+    "OpenSans-MediumItalic": require("../assets/fonts/OpenSans-MediumItalic.ttf"),
+    "OpenSans-SemiBold": require("../assets/fonts/OpenSans-SemiBold.ttf"),
+    "OpenSans-SemiBoldItalic": require("../assets/fonts/OpenSans-SemiBoldItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
 }
